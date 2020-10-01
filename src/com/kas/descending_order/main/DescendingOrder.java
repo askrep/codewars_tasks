8568ddb20f15bcbc874e9e0df3935e1420f80f42
Alexander <alexandskr@gmail.com>
2020-10-01 16:24:07 +0300

    Updated

--- src/com/kas/descending_order/main/DescendingOrder.java
+++ src/com/kas/descending_order/main/DescendingOrder.java
@@ -2,17 +2,13 @@ package com.kas.descending_order.main;
 
 import java.util.Comparator;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.Comparator.reverseOrder;
 
 public class DescendingOrder {
     /**
      * Your task is to make a function that can take any non-negative
      * integer as an argument and return it with its digits in descending order.
      * Essentially, rearrange the digits to create the highest possible number.
-     * <p>
+     *
      * Examples:
      * Input: 42145 Output: 54421
      * Input: 145263 Output: 654321
@@ -21,17 +17,15 @@ public class DescendingOrder {
 
     public static void main(String[] args) {
         // write your code here
-        sortDesc(123456789);
+        System.out.println(sortDesc(123456789));
     }
 
-    public static int sortDesc(int i) {
+    public static int sortDesc(final int num) {
 
-        String result = Integer.toString(i).chars()
+        return Integer.parseInt(Integer.toString(num).chars()
                 .boxed()
                 .map(obj -> String.valueOf(obj - '0'))
-                .sorted(reverseOrder())
-                .collect(Collectors.joining());
-
-        return Integer.valueOf(result);
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining()));
     }
 }
